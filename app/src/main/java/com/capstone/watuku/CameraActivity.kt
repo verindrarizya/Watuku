b73f488adfb692ee031e4f39930864e62aa8e372
verindrarizya <v.rizya22@gmail.com>
2021-05-20 22:52:25 +0700

    Layouting the Camera layout & add camera features such as use cases for camerax and define output file

--- app/src/main/java/com/capstone/watuku/CameraActivity.kt
+++ app/src/main/java/com/capstone/watuku/CameraActivity.kt
@@ -1,11 +1,174 @@
 package com.capstone.watuku
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaScannerConnection
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.webkit.MimeTypeMap
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.net.toFile
+import com.capstone.watuku.databinding.ActivityMainBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CameraActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var imageCapture: ImageCapture? = null
+
+    private lateinit var outputDirectory: File
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Remove the AppBar
+        supportActionBar?.hide()
+
+        // Request Camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        binding.fabCameraCapture.setOnClickListener { takePhoto() }
+
+        outputDirectory = getOutputDirectory()
     }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // CameraX Preview Use Case
+            val preview = Preview.Builder()
+                .build()
+                .also { it.setSurfaceProvider(binding.viewFinder.surfaceProvider) }
+
+            // CameraX ImageCapture Use Case
+            imageCapture = ImageCapture.Builder().build()
+
+            // Select the back camera
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+
+                // Bind use case to camera
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            } catch (e: Exception) {
+
+            }
+        }, ContextCompat.getMainExecutor(this)) // Execute operation on Main Thread
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+
+        val photoFile = File(outputDirectory, fileBasedOnTimeItTaken())
+        // Create output options object that contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        // Set image capture listener, that gonna be triggered when photo is taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val savedUri = outputFileResults.savedUri ?: Uri.fromFile(photoFile)
+                    val msg = "Success Capture Photo"
+
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, "onImageSaved: $savedUri")
+
+                    // Announce to other app like Gallery there's new image in storage
+                    triggerMediaScanner(savedUri)
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(TAG, "onError: ${exception.message}", exception)
+                }
+
+            })
+    }
+
+    private fun allPermissionsGranted(): Boolean {
+        return REQUIRED_PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply {
+                mkdirs()
+            }
+        }
+
+        return if (mediaDir != null && mediaDir.exists()) mediaDir else filesDir
+    }
+
+    // create output file name based on time it was taken
+    private fun fileBasedOnTimeItTaken(): String {
+        val fileName = SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis())
+        val file = fileName + FILE_EXTENSIONS
+        return file
+    }
+
+    // Manually trigger media scanner so other app know there's a new image
+    private fun triggerMediaScanner(savedUri: Uri) {
+        val mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(savedUri.toFile().extension)
+
+        // Trigger media scanner
+        MediaScannerConnection.scanFile(
+            applicationContext,
+            arrayOf(savedUri.toFile().absolutePath),
+            arrayOf(mimeType)
+        ) { _, uri ->
+            Log.d(TAG, "Scanned on storage: $uri ")
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this, "Permissions not granted", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+
+    companion object {
+        private const val TAG = "WatukuCameraActivity"
+
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val FILE_EXTENSIONS = ".jpg"
+
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
 }
